09d495d4b6e462b5950e5f49137b1a584f6191ed
jiro <tigeeet@gmail.com>
2024-05-31 23:48:34 +0300

    create week component

---
+++ src/components/week/week.tsx
@@ -0,0 +1,36 @@
+import { cn } from "@bem-react/classname";
+import React, { memo } from "react";
+import "./week.css";
+import { createDateRange } from "@utils/createDateRange";
+import { WEEKDAY } from "@utils/getWeekdayName";
+import Todo from "../todo/todo";
+import { useDate } from "@hooks/useDate";
+import { getFirstDayOfWeek } from "@utils/getFirstDayOfWeek";
+import { getLastDayOfWeek } from "@utils/getLastDayOfWeek";
+
+const cnWeek = cn("week");
+const Week = () => {
+  const { date } = useDate();
+  const firstDay = getFirstDayOfWeek(date);
+  const lastDay = getLastDayOfWeek(date);
+  const dateRange = createDateRange(firstDay, lastDay);
+
+  return (
+    <div className={cnWeek()}>
+      {WEEKDAY.map((weekday) => (
+        <span key={weekday} className={cnWeek("weekday")}>
+          {weekday}
+        </span>
+      ))}
+      {dateRange.map((d) => (
+        <Todo
+          limit={29}
+          date={d}
+          key={d.toISOString()}
+          isFiller={date.getMonth() !== d.getMonth()}
+        />
+      ))}
+    </div>
+  );
+};
+export default memo(Week);
