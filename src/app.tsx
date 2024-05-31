1d1b9198823abe5136c94e7ea31630d294d0b70f
jiro <tigeeet@gmail.com>
2024-05-31 17:05:21 +0300

    добавил addTodo, deleteTodo, toggleTodo

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,44 @@
-import React, { memo } from "react";
+import React, { memo, useCallback, useState } from "react";
+import { useLocalStorage } from "./hooks/useLocalStorage";
+import { makeId } from "./utils/makeId";
+import Calendar from "./components/calendar/calendar";
+import { Todo } from "./types";
+
 const App = () => {
+  const [todos, setTodos] = useLocalStorage<Todo[]>({
+    key: "todos",
+    defaultValue: [],
+  });
+
+  const handleAddTodo = useCallback(
+    (payload: Pick<Todo, "title" | "description">) => {
+      setTodos((todos) => [
+        ...todos,
+        {
+          id: makeId(),
+          createdAt: new Date(),
+          completed: false,
+          ...payload,
+        },
+      ]);
+    },
+    [setTodos]
+  );
+
+  const handleDeleteTodo = useCallback(
+    (id: string) => setTodos((todos) => todos.filter((todo) => todo.id !== id)),
+    [setTodos]
+  );
+
+  const handleToggleTodo = useCallback(
+    (id: string, completed: boolean) => {
+      setTodos((todos) =>
+        todos.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
+      );
+    },
+    [setTodos]
+  );
+
   return <></>;
 };
 export default memo(App);
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type Todo = {
+  id: string;
+  createdAt: Date;
+  completed: boolean;
+  title: string;
+  description: string;
+};
