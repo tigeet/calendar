5c072ea2f29400c1ee227d47fc84ea29a98aeebe
jiro <tigeeet@gmail.com>
2024-05-31 23:46:12 +0300

    add global styles and configs

--- src/app.tsx
+++ src/app.tsx
@@ -1,44 +1,21 @@
-import React, { memo, useCallback, useState } from "react";
-import { useLocalStorage } from "./hooks/useLocalStorage";
-import { makeId } from "./utils/makeId";
-import Calendar from "./components/calendar/calendar";
-import { Todo } from "./types";
+import React, { memo } from "react";
+import { TodoProvider } from "./providers/todoProvider";
+import Main from "./pages/main/main";
+import "./app.css";
+import "./reset.css";
+import { ModalProvider } from "./providers/modalProvider";
+import { DateProvider } from "./providers/dateProvider";
 
 const App = () => {
-  const [todos, setTodos] = useLocalStorage<Todo[]>({
-    key: "todos",
-    defaultValue: [],
-  });
-
-  const handleAddTodo = useCallback(
-    (payload: Pick<Todo, "title" | "description">) => {
-      setTodos((todos) => [
-        ...todos,
-        {
-          id: makeId(),
-          createdAt: new Date(),
-          completed: false,
-          ...payload,
-        },
-      ]);
-    },
-    [setTodos]
+  return (
+    <TodoProvider>
+      <ModalProvider>
+        <DateProvider>
+          <Main />
+        </DateProvider>
+      </ModalProvider>
+    </TodoProvider>
   );
-
-  const handleDeleteTodo = useCallback(
-    (id: string) => setTodos((todos) => todos.filter((todo) => todo.id !== id)),
-    [setTodos]
-  );
-
-  const handleToggleTodo = useCallback(
-    (id: string, completed: boolean) => {
-      setTodos((todos) =>
-        todos.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
-      );
-    },
-    [setTodos]
-  );
-
-  return <></>;
 };
+
 export default memo(App);
